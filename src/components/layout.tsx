534785bd491a6a2f7276dc8a434145ac8e768ccd
fabioportieri <fazfazzi@gmail.com>
2023-09-18 12:49:41 +0200

    redeploy

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,13 +1,14 @@
-import Header from "./header"
-import Footer from "./footer"
-import type { ReactNode } from "react"
+import type { ReactNode } from "react";
+import Footer from "./footer";
+import Header from "./header";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <>
       <Header />
       <main>{children}</main>
+
       <Footer />
     </>
-  )
+  );
 }
