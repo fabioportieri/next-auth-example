9fe99a95fadad87319a7e32ea8f14995fee22c22
fabioportieri <fazfazzi@gmail.com>
2023-09-18 12:58:35 +0200

    updated next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import("next").NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  env: {
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    MONGODB_URI: process.env.MONGODB_URI,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    AUTH_KEYCLOAK_SECRET: process.env.AUTH_KEYCLOAK_SECRET,
+    AUTH_KEYCLOAK_ISSUER: process.env.AUTH_KEYCLOAK_ISSUER,
+    AUTH_KEYCLOAK_ID: process.env.AUTH_KEYCLOAK_ID
+  }
 }
